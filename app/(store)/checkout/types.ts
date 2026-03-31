a713f213dcb732a9d368655ad6d0381d93c614f3
Sadiq <sadiqy93@gmail.com>
2026-03-30 22:05:16 -0400

    removed unused types

--- app/(store)/checkout/types.ts
+++
@@ -1,8 +0,0 @@
-// app/(store)/checkout/types.ts
-export type CheckoutActionState = {
-  success: boolean | null;
-  message: string | null;
-  fieldErrors: Record<string, string[] | undefined>;
-  orderId?: string;
-  paymentUrl?: string;
-};
