61ed25ea0d693367a1db7ca37ba20ce8cb25990f
Sadiq <sadiqy93@gmail.com>
2026-04-09 02:00:38 -0400

    update schema

--- components/cart/CartDrawer.tsx
+++ components/cart/CartDrawer.tsx
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+ 'use client'
+
+ import { useEffect } from "react";
 import { X } from "lucide-react";
 import { useCart } from "@/lib/cart/store";
 import { Button } from "@/components/ui/Button";
